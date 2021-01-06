e7482828f9cbdc157b9f5d9fca3a9727c203ce77
klnpc26 <kaiquelin12.kl@gmail.com>
2021-01-06 18:16:39 -0300

    Front-Web part 1

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,23 @@
 import './styles.css';
-import { ReactComponent as Logo} from './logo.svg';
 import StepsHeader from './StepsHeader';
+import ProductList from './ProductList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
-function Orders(){
+function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products)
+
+    useEffect(() => {
+        fetchProducts().then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductList products={products}/>
         </div>
     )
 }
